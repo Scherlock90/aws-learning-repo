2148fe6c4e88eb46c10c7aef13589bc67d3031c8
Sebastian Napora <sna@spyro-soft.com>
2023-04-24 12:36:41 +0200

    feat: added client application

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        It's a testing app for checking AWS deployment process
+      </header>
+    </div>
+  );
+}
+
+export default App;
